1e9f5d7bfebf67ea2ad1b9fc31fab156a1118c8c
Shoyadbek <sh.abdurasulov@gmail.com>
2024-01-30 17:34:13 +0500

    post and back button

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,44 +1,52 @@
-'use client'
+'use client';
 
 import { useTelegram } from '@/app/telegram.provider';
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-async function fetchData(id) {
-    const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
-    const result = await res.json();
-    return result;
-  }
-  
+const Post = ({ params }) => {
+  const telegram = useTelegram();
+  const router = useRouter();
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    const fetchData = async (id) => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
+      const result = await res.json();
+      return result;
+    };
 
-const Post = ({params}) => {
+    const setupBackButton = () => {
+      telegram.BackButton.show().onClick(() => {
+        router.back();
+      });
+    };
 
-    const telegram = useTelegram()
-    const [data, setData] = useState(null);
+    // Вызываем setupBackButton
+    setupBackButton();
 
-    useEffect(() => {
-    
-        fetchData(params.id).then((result) => {
-          setData(result);
-        })
+    // Вызываем fetchData
+    fetchData(params.id).then((result) => {
+      setData(result);
+    });
 
-        telegram.BackButton.show();
-    
-      }, [params.id, telegram.BackButton])
+    // Возвращаем функцию для выполнения при размонтировании компонента
+    return () => {
+      telegram.BackButton.offClick();
+      telegram.BackButton.hide();
+    };
+  }, [router, params.id, telegram.BackButton]);
 
-    return (
+  return (
+    <>
+      {data && (
         <>
-            {data && (<>
-
-                <h2 className="text-3xl font-bold tracking-tight text-white-900 sm:text-4xl">  {data.title}</h2>
-                <p className="mt-2 text-lg leading-8 text-white-600">
-                {data.body}
-                </p>
-               
-            
-            </>)}
+          <h2 className="text-3xl font-bold tracking-tight text-white-900 sm:text-4xl">{data.title}</h2>
+          <p className="mt-2 text-lg leading-8 text-white-600">{data.body}</p>
         </>
-    )
-}
+      )}
+    </>
+  );
+};
 
 export default Post;
